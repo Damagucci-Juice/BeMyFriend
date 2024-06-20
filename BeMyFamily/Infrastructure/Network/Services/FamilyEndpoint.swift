afcf1a4b596771325a5f930e007e2ba08ed9d8c2
Damagucci-Juice <jsr9874@gmail.com>
2024-06-20 18:07:41 +0900

    refactor: 불필요한 별도의 Extension 파일 제거

--- BeMyFamily/Infrastructure/Network/Services/FamilyEndpoint.swift
+++ BeMyFamily/Infrastructure/Network/Services/FamilyEndpoint.swift
@@ -15,6 +15,7 @@ enum FamilyEndpoint {
     case animal(filteredItem: AnimalFilter, page: Int)
 }
 
+// MARK: - For URL Ingredients, Path, Params, Method etc..
 extension FamilyEndpoint {
     var baseURL: URL {
         // swiftlint:disable force_unwrapping
@@ -71,5 +72,38 @@ extension FamilyEndpoint {
 
     var headers: [String: String]? {
         return nil
+    }
+}
+
+// MARK: - For Network Resources, URL
+extension FamilyEndpoint {
+    func makeURL() -> URL {
+        let urlString = baseURL.absoluteString + path
+        guard let url = URL(string: urlString) else {
+            fatalError("Failed to create URL for endpoint: \(urlString)")
+        }
+        guard let addedParameterURL = makeURLComponent(url).url else {
+            fatalError("Failed to create URL for endpoint: \(urlString)")
+        }
+        return addedParameterURL
+    }
+
+    func makeURLComponent(_ url: URL) -> URLComponents {
+        guard var component = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
+            fatalError("Failed to create URLComponents for endpoint: \(baseURL)")
+        }
+        component.queryItems = parameter.map { (key, value) in
+            URLQueryItem(name: key, value: value)
+        }
+        component.percentEncodedQuery = component.percentEncodedQuery?.replacingOccurrences(of: "%25", with: "%")
+        return component
+    }
+
+    func makeURLRequest() -> URLRequest {
+        let url = makeURL()
+        var request = URLRequest(url: url)
+        request.httpMethod = method
+        request.allHTTPHeaderFields = headers
+        return request
     }
 }
